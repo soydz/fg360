6209df82b78200c75725e403a97148d9ca2f563b
soydz <dev@soydz.com>
2025-09-20 12:15:26 -0500

    fix(entities): add @Builder.Default to initialize collections in User, Role and permission

--- src/main/java/com/fleetguard360/persistence/entity/Role.java
+++ src/main/java/com/fleetguard360/persistence/entity/Role.java
@@ -23,9 +23,11 @@ public class Role {
     private RoleEnum role;
 
     @ManyToMany(mappedBy = "roles")
+    @Builder.Default
     private Set<User> users = new HashSet<>();
 
     @ManyToMany
     @JoinTable(name = "role_permission", joinColumns = @JoinColumn(name = "role_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    @Builder.Default
     private Set<Permission> permissions = new HashSet<>();
 }
--- src/main/java/com/fleetguard360/persistence/entity/User.java
+++ src/main/java/com/fleetguard360/persistence/entity/User.java
@@ -39,5 +39,6 @@ public class User {
 
     @ManyToMany
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @Builder.Default
     private Set<Role> roles = new HashSet<>();
 }
